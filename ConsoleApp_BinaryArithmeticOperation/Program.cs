a0f17d931831c24dcef01b480617c07bbedf06ee
Yervand.Marikyan <yervand.marikyan@gmail.com>
2021-07-05 17:41:50 +0400

    first commit

---
+++ ConsoleApp_BinaryArithmeticOperation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp_BinaryArithmeticOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter Arithmetic Operation");
+        start: string s = ReadLine();
+            string[] nshan = s.Split('-', '+', '*', '/');
+            int a = int.Parse(nshan[0]);
+            int b = int.Parse(nshan[1]);
+
+            int x = nshan[0].Length;
+            switch (s[x])
+            {
+                case '+':
+                    WriteLine(a + b);
+                    break;
+                case '-':
+                    WriteLine(a - b);
+                    break;
+                case '*':
+                    WriteLine(a * b);
+                    break;
+                case '/':
+                    if (b != 0)
+                    {
+                        WriteLine(a / b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Division by zero");
+                    }
+                    break;
+            }
+            Console.WriteLine(new string('-', 10));
+            goto start;
+        }
+    }
+}
